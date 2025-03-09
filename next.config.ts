15d0767c98b87de73272d792521f4f5af488a0fb
Edward Twumasi <createdliving1000@gmail.com>
2025-03-09 23:59:24 +0000

    home redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/", // When someone visits the root
+        destination: "/home", // Redirect to /home
+        permanent: true, // Use 308 for permanent redirect (or false for 307 temporary)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
